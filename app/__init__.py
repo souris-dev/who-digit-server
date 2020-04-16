cdd14a4a11dba1ef438374975003ed68f39dc8b7
souris-dev <suorees113@gmail.com>
2020-04-16 15:43:23 +0530

    Set Debug configuration to true

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,5 @@ app = Flask(__name__, instance_relative_config=True)
 # Load the views
 from app import views
 
-# Load the config file
-app.config.from_object('config')
+# Set debug to true
+app.config['DEBUG'] = True
